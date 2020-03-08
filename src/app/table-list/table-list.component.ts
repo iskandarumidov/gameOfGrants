aebdb4aca2e3a46e62f2c19219f1434a649a1ea0
Iskandar <iskander.umidov.iu@gmail.com>
2020-03-08 06:18:06 +0000

    form new bind2

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -19,7 +19,7 @@ export class TableListComponent implements OnInit {
   onClickSubmit(data){
     alert(data.cost);
     console.log(data.cost);
-    this.dataService.addCard(data.cost, "asdf", "asdf", "asdf", "adsf", true);
+    this.dataService.addCard(data.cost, data.name, data.title, data.researcher, data.donor, data.isProposal);
   }
 
   
